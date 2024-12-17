af33e57eb40664fe07df9fc9d800e108ebbaf8b8
CyrilusK <kcyrg17@gmail.com>
2024-12-17 19:17:21 +0300

    create list

---
+++ Landmarks/Model/ModelData.swift
@@ -0,0 +1,30 @@
+//
+//  ModelData.swift
+//  Landmarks
+//
+//  Created by Cyril Kardash on 17.12.2024.
+//
+
+import SwiftUI
+
+var landmarks: [Landmark] = load("landmarkData.json")
+
+func load<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+    
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil) else {
+        fatalError("Couldn't find \(filename)")
+    }
+    
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't load \(filename): \(error)")
+    }
+    
+    do {
+        return try JSONDecoder().decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't decode \(filename): \(error)")
+    }
+}
---
+++ Landmarks/View/LandmarkList.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Cyril Kardash on 17.12.2024.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkList()
+}
